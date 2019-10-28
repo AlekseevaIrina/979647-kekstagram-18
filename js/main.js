35d2f48426e97ba9c89da42e798fb5fc91953f60
Irina Alekseeva <50372200+AlekseevaIrina@users.noreply.github.com>
2019-10-28 23:03:00 +0300

    Add effects and validation

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var SHOWN_PICTURES = 25;
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var NAMES = ['Валера', 'Олег', 'Василий', 'Мария', 'Анна', 'Анатолий'];
+var ESC_KEYCODE = 27;
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -108,3 +109,186 @@ pictures.forEach(function (picture) {
 });
 
 picturesList.appendChild(fragment);
+
+//  новое задание
+var uploadFile = document.querySelector('#upload-file');
+var editForm = document.querySelector('.img-upload__overlay');
+var editFormCancel = editForm.querySelector('#upload-cancel');
+var effectLevelPin = editForm.querySelector('.effect-level__pin');
+var effectsRadio = editForm.querySelectorAll('.effects__radio');
+var previewImg = editForm.querySelector('.img-upload__preview');
+
+var currentEffect = 'effect-none';
+
+//  пока статична, т.к. ползунок еще не двигается
+var pinPosition = 0.2;
+
+var onEditFormEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeEditForm();
+  }
+};
+
+var showEditForm = function () {
+  editForm.classList.remove('hidden');
+  document.addEventListener('keydown', onEditFormEscPress);
+
+  currentEffect = 'effect-none';
+  previewImg.className = 'effects__preview--' + currentEffect;
+  previewImg.style.filter = getSaturationFilter();
+};
+
+var closeEditForm = function () {
+  editForm.classList.add('hidden');
+  uploadFile.value = '';
+  document.removeEventListener('keydown', onEditFormEscPress);
+};
+
+uploadFile.addEventListener('change', showEditForm);
+
+editFormCancel.addEventListener('click', closeEditForm);
+
+//  применение фильтров
+var getSaturationFilter = function () {
+
+  var saturationFilter = 'none';
+
+  switch (currentEffect) {
+    case 'chrome': {
+      saturationFilter = 'grayscale(' + pinPosition + ')';
+      break;
+    }
+    case 'sepia': {
+      saturationFilter = 'sepia(' + pinPosition + ')';
+      break;
+    }
+    case 'marvin': {
+      saturationFilter = 'invert(' + pinPosition * 100 + '%)';
+      break;
+    }
+    case 'phobos': {
+      saturationFilter = 'blur(' + pinPosition * 3 + 'px)';
+      break;
+    }
+    case 'heat': {
+      saturationFilter = 'brightness(' + pinPosition * 3 + ')';
+      break;
+    }
+    default:
+      saturationFilter = 'none';
+  }
+  return saturationFilter;
+};
+
+var addClass = function (effect) {
+  previewImg.className = 'effects__preview--' + effect.value;
+  currentEffect = effect.value;
+  pinPosition = 1;
+  previewImg.style.filter = getSaturationFilter();
+};
+
+effectsRadio.forEach(function (effect) {
+  effect.addEventListener('click', function () {
+    addClass(effect);
+  });
+});
+
+var setSaturationLevel = function () {
+  previewImg.style.filter = getSaturationFilter();
+};
+
+effectLevelPin.addEventListener('mouseup', setSaturationLevel);
+
+//  изменение размера
+var SCALE_STEP = 25;
+var scaleControlSmaller = editForm.querySelector('.scale__control--smaller');
+var scaleControlBigger = editForm.querySelector('.scale__control--bigger');
+var scaleControlValue = editForm.querySelector('.scale__control--value');
+
+scaleControlValue.value = '100%';
+var currentScaleControl = 100;
+
+var changeSize = function (isReduce) {
+  if (isReduce) {
+    currentScaleControl = currentScaleControl - SCALE_STEP;
+  } else {
+    currentScaleControl = currentScaleControl + SCALE_STEP;
+  }
+  scaleControlValue.value = currentScaleControl + '%';
+  previewImg.style.transform = 'scale(' + currentScaleControl / 100 + ')';
+};
+
+var reduceSize = function () {
+  if (currentScaleControl > 25) {
+    changeSize(true);
+  }
+};
+
+var increaseSize = function () {
+  if (currentScaleControl < 100) {
+    changeSize(false);
+  }
+};
+
+scaleControlSmaller.addEventListener('click', reduceSize);
+scaleControlBigger.addEventListener('click', increaseSize);
+
+//  валидация хэштегов
+var hashtagField = document.querySelector('.text__hashtags');
+
+var isHashSymbolFirst = function (hashtag) {
+  if (hashtag[0] === '#') {
+    return true;
+  }
+  return false;
+};
+
+var isHashtagsSame = function (hashtags) {
+  var array = [];
+
+  for (var i = 0; i < hashtags.length; i++) {
+    if (array.includes(hashtags[i].toLowerCase())) {
+      return true;
+    }
+    array.push(hashtags[i].toLowerCase());
+  }
+
+  return false;
+
+};
+
+hashtagField.addEventListener('invalid', function () {
+  if (!hashtagField.validity.valid) {
+    hashtagField.setCustomValidity(hashtagField.validationMessage);
+  }
+});
+
+var checkedHashtags = function () {
+
+  var hashtags = hashtagField.value.split(' ');
+
+  hashtags.forEach(function (hashtag) {
+    if (!isHashSymbolFirst(hashtag)) {
+      hashtagField.setCustomValidity('Хэш-тег должен начинаться с символа #');
+      return;
+    } else if (hashtag.length === 1) {
+      hashtagField.setCustomValidity('Хэш-тег не может состоять только из одной решётки');
+      return;
+    } else if (hashtag.length > 20) {
+      hashtagField.setCustomValidity('Максимальная длина одного хэш-тега 20 символов, включая решётку');
+      return;
+    } else {
+      hashtagField.setCustomValidity('');
+    }
+  });
+
+  if (hashtags.length > 5) {
+    hashtagField.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+    return;
+  } else if (isHashtagsSame(hashtags)) {
+    hashtagField.setCustomValidity('Нельзя использовать повторяющиеся хэш-теги');
+  }
+
+};
+
+hashtagField.addEventListener('input', checkedHashtags);
