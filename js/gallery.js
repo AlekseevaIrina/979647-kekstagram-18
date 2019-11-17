af1d3d3224aeac8d32cb93e43c49d05703a68fcb
Irina Alekseeva <50372200+AlekseevaIrina@users.noreply.github.com>
2019-11-17 13:18:02 +0300

    Add filters

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,18 @@
 'use strict';
 
 (function () {
-  //  обработчик успешной загрузки фотографий с сервера
-  var onSuccess = function (allPictures) {
+  var DEBOUNCE_INTERVAL = 500;
+
+  var imgFilters = document.querySelector('.img-filters');
+  var popularButton = imgFilters.querySelector('#filter-popular');
+  var randomButton = imgFilters.querySelector('#filter-random');
+  var discussedButton = imgFilters.querySelector('#filter-discussed');
+
+  var picturesCopy = [];
+
+  window.updatePictures = function (allPictures) {
+
+    removePictures();
 
     var pictures = window.data.createPictures(allPictures);
     var fragment = document.createDocumentFragment();
@@ -13,7 +23,37 @@
     });
 
     picturesList.appendChild(fragment);
+  };
 
+  //  удаление уже отрисованных фотографий
+  var removePictures = function () {
+    var pictures = document.querySelectorAll('.picture');
+
+    pictures.forEach(function (picture) {
+      picture.remove();
+    });
+  };
+
+  //  обработчик успешной загрузки фотографий с сервера
+  var onSuccess = function (pictures) {
+
+    picturesCopy = JSON.parse(JSON.stringify(pictures));
+
+    window.updatePictures(pictures);
+
+    imgFilters.classList.remove('img-filters--inactive');
+
+    popularButton.addEventListener('click', window.util.debounce(function () {
+      window.filters.onPopularButtonClick(picturesCopy, popularButton);
+    }, DEBOUNCE_INTERVAL));
+
+    randomButton.addEventListener('click', window.util.debounce(function () {
+      window.filters.onRandomButtonClick(pictures, randomButton);
+    }, DEBOUNCE_INTERVAL));
+
+    discussedButton.addEventListener('click', window.util.debounce(function () {
+      window.filters.onDiscussedButtonClick(pictures, discussedButton);
+    }, DEBOUNCE_INTERVAL));
   };
 
   window.api.load(onSuccess, window.api.onServerRequestError);
